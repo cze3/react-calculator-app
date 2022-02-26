d0449e208b5fd929a4b8aaf1331fc65c57414eb5
Mustafa <mustafaalamarah.hotfish@gmail.com>
2022-02-26 13:51:07 +0300

    react calculator app complete with theme buttons issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
+import Header from './components/Header';
+import Buttons from './components/Buttons';
 function App() {
+  const [result, setResult] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='calculator'>
+        <Header />
+        <form>
+          <input type="text" value={result} className='user-input' />  
+        </form>
+        <Buttons setResult={setResult} result={result} />
+      </div>
+    </>
   );
 }
 
